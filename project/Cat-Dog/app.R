62d950c6e92fff2f9ca51aa9cc8bf6334fbe7643
madiyonash <madison.yonash@yahoo.com>
2024-04-04 11:13:27 -0400

    adding shiny file

---
+++ project/Cat-Dog/app.R
@@ -0,0 +1,53 @@
+#
+# https://shiny.posit.co/
+#
+
+library(shiny)
+library(keras)
+
+# Load the model
+model <- load_model_tf("cnn-mnist/")
+
+# Define the UI
+ui <- fluidPage(
+  # App title ----
+  titlePanel("Cats and Dogs!"),
+  # Sidebar layout with input and output definitions ----
+  sidebarLayout(
+    # Sidebar panel for inputs ----
+    sidebarPanel(
+      # Input: File upload
+      fileInput("image_path", label = "Input a JPEG image")
+    ),
+    # Main panel for displaying outputs ----
+    mainPanel(
+      # Output: Histogram ----
+      textOutput(outputId = "prediction"),
+      plotOutput(outputId = "image")
+    )
+  )
+)
+
+# Define server logic required to draw a histogram ----
+server <- function(input, output) {
+  
+  image <- reactive({
+    req(input$image_path)
+    jpeg::readJPEG(input$image_path$datapath)
+  })
+  
+  output$prediction <- renderText({
+    
+    img <- image() %>% 
+      array_reshape(., dim = c(1, dim(.), 1))
+    
+    paste0("The predicted class is ", predict_classes(model, img))
+  })
+  
+  output$image <- renderPlot({
+    plot(as.raster(image()))
+  })
+  
+}
+
+shinyApp(ui, server)
